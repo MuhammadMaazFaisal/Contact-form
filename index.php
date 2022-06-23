eec0d9739abfc2877e5673f1a666a5ac47df9676
M.Maaz Faisal <m.maazfaisal0301@gmail.com>
2022-06-23 22:53:08 +0500

    added files

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Contact Form</title>
+</head>
+
+<body class="row bg-">
+<div class="d-flex col-4 ml-5 justify-content-end ">
+        <img class='py-5' src="signup-image.jpg">
+    </div>
+    <div class="container my-3 mx-5 px-5 col">
+        <div class="my-5">
+            <div class="text-primary">
+                <h1 class="fw-bold">Tell us about your issue</h1>
+            </div>
+            <div class="">
+                <form action="view.php" method="POST">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" class="form-control" required value="<?php if (isset($_SESSION['name'])) echo $_SESSION['name'] ?>">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" class="form-control" required value="<?php if (isset($_SESSION['email'])) echo $_SESSION['email'] ?>">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="issue">Your issue: </label>
+                        <select name="issue" id="si" class="form-control" value="<?php if (isset($_SESSION['issue'])) echo $_SESSION['issue'] . ' selected' ?>">
+                            <option value="query" <?php if (isset($_SESSION['issue']) && $_SESSION['issue'] == 'query') echo "selected" ?>>Query</option>
+                            <option value="feedback" <?php if (isset($_SESSION['issue']) && $_SESSION['issue'] == 'feedback') echo "selected" ?>>Feedback</option>
+                            <option value="complaint" <?php if (isset($_SESSION['issue']) && $_SESSION['issue'] == 'complaint') echo "selected" ?>>Complaint</option>
+                            <option value="other" <?php if (isset($_SESSION['issue']) && $_SESSION['issue'] == 'other') echo "selected" ?>>Other</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control" id="description" name="description" rows="3"><?php if (isset($_SESSION['description'])) echo $_SESSION['description'] ?></textarea>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end mr-4">
+                    <button type="submit" name="submit" class="btn btn-primary px-3">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
+</html>
---
+++ view.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Show</title>
+</head>
+<body>
+    
+
+
+<?php
+
+session_start();
+
+$name = '';
+$email = '';
+$issue = '';
+$description = '';
+
+if ( isset($_POST['submit'])
+        )
+     {
+        $name = $_SESSION['name'] = $_POST['name'];
+        $email = $_SESSION['email'] = $_POST['email'];
+        $issue = $_SESSION['issue'] = $_POST['issue'];
+        $description = $_POST['description'];
+    }
+?>
+<div class="container">
+    <div class="card-header my-5">
+        <h3>Your information: </h3>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <td><?php echo $name?></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><?php echo $email ?></td>
+                </tr>
+                <tr>
+                    <th>Issue</th>
+                    <td><?php echo $issue ?></td>
+                </tr>
+                <tr>
+                    <th>Descripiton</th>
+                    <td><?php echo $description ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end mr-4">
+    <a class="btn btn-primary px-4" href="changes.php" role="button">Edit</a>
+    </div>
+</div>
+
+
+
+</body>
+</html>
