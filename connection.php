c6ef4025d5ab23077ea199ecc058dc3449019f81
M.Maaz Faisal <m.maazfaisal0301@gmail.com>
2022-09-14 01:24:39 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "contact-form";
+$server = "sql209.epizy.com";
+$username = "epiz_32572167";
+$password = "aCnbtATt3OG5R7";
+$dbname = "epiz_32572167_contactform";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+$conn = new mysqli($server, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
